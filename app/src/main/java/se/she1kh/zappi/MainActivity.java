fe68a958c94493cff07f3d10f4eaeaa9b0503c30
Usman Sheikh <ussiling@live.com>
2015-09-11 19:52:21 +0200

    Api added

--- app/src/main/java/se/she1kh/zappi/MainActivity.java
+++ app/src/main/java/se/she1kh/zappi/MainActivity.java
@@ -1,9 +1,22 @@
 package se.she1kh.zappi;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +24,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new RequestTask().execute("http://stackoverflow.com");
+    }
+    class RequestTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... uri) {
+
+            try {
+               getJson();
+
+            } catch (IOException e) {
+                //TODO Handle problems..
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //Do anything with response..
+        }
+    }
+    public void getJson() throws IOException {
+        URL yahoo = null;
+        try {
+            yahoo = new URL("http://www.myapifilms.com/imdb/top");
+
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        yc.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+            Log.i("HOSSIIII","TEST "+inputLine);
+        in.close();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     @Override
