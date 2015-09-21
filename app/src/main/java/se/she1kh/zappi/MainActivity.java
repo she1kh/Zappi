641d6c885c2e47c1901dbefef60c825874382224
Usman Sheikh <ussiling@live.com>
2015-09-21 21:42:43 +0200

    Api added2

--- app/src/main/java/se/she1kh/zappi/MainActivity.java
+++ app/src/main/java/se/she1kh/zappi/MainActivity.java
@@ -24,7 +24,6 @@ import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
